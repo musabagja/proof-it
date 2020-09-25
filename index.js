c1aa5b424f739358fa29f777a7e27c76d6010a3d
musabagja <bagja7musa@gmail.com>
2020-09-25 14:48:31 +0700

    First Maintenance

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ let marvelButton=document.getElementById('marvel-button');
 let dcButton=document.getElementById('dc-button');
 let nextOrder=document.getElementById('nextOrder');
 marvelButton.onclick=function() {
-    startButton.action="/marvel.html";
+    startButton.action="./marvel.html";
     nextOrder.innerText="Click the start button!!";
 }
 dcButton.onclick=function() {
-    startButton.action="/dc.html";
+    startButton.action="./dc.html";
     nextOrder.innerText="Click the start button!!";
 }
